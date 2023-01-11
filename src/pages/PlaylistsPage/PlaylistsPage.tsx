f5e9d57861c319bd5f9c1e789e945353518a8216
Павел <58857483+Haskiro@users.noreply.github.com>
2023-01-11 13:30:29 +0300

    search panel and filters added to playlists page

--- src/pages/PlaylistsPage/PlaylistsPage.tsx
+++ src/pages/PlaylistsPage/PlaylistsPage.tsx
@@ -1,12 +1,17 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 import Spinner from "@components/Spinner";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { fetchPlaylists } from "@store/slices/playlistsSlice";
+import { Input } from "antd";
+import cn from "classnames";
 import { Link } from "react-router-dom";
 import styles from "./PlaylistsPage.module.scss";
+const { Search } = Input;
 
 const PlaylistsPage: FC = () => {
 	const dispatch = useAppDispatch();
+	const [sortByAlphabet, setSortByAlphabet] = useState<boolean>(true);
+	const [searchValue, setSearchValue] = useState<string>("");
 	const playlists = useAppSelector((state) => state.playlists.playlists);
 	const status = useAppSelector((state) => state.playlists.status);
 
@@ -14,31 +19,87 @@ const PlaylistsPage: FC = () => {
 		dispatch(fetchPlaylists());
 	}, []);
 
+	const filteredPlaylists = useMemo(() => {
+		let filteredPlaylists = playlists?.slice();
+
+		if (searchValue) {
+			filteredPlaylists = filteredPlaylists?.filter((playlist) =>
+				playlist.title.toLowerCase().includes(searchValue)
+			);
+		}
+
+		if (!sortByAlphabet) return filteredPlaylists;
+		return filteredPlaylists?.sort((a, b) => {
+			const textA = a.title.toUpperCase();
+			const textB = b.title.toUpperCase();
+
+			return textA < textB ? -1 : textA > textB ? 1 : 0;
+		});
+	}, [playlists, sortByAlphabet, searchValue]);
+
+	const onSearch = (searchValue: string) => {
+		setSearchValue(searchValue.toLowerCase());
+	};
+
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.heading}>Плейлисты</h1>
+			<h1 className={styles.heading}>Плейлисты </h1>
+			<div className={styles.controlPanel}>
+				<Search
+					className={styles.controlInput}
+					placeholder="Введите название плейлиста"
+					allowClear
+					enterButton="Поиск"
+					size="large"
+					onSearch={onSearch}
+				/>
+				<div className={styles.controlButtons}>
+					<button
+						className={cn(styles.controlButtonsItem, {
+							[styles.controlButtonsItemChecked]: sortByAlphabet,
+						})}
+						onClick={() =>
+							setSortByAlphabet((prevState) => !prevState)
+						}
+					>
+						По Алфавиту
+					</button>
+					<button
+						className={styles.controlButtonsItem}
+						onClick={() => dispatch(fetchPlaylists())}
+					>
+						Обновить
+					</button>
+				</div>
+			</div>
 			{status === "succeeded" ? (
-				<ul className={styles.list}>
-					{playlists?.map((playlist) => (
-						<li className={styles.item} key={playlist.id}>
-							<div className={styles.card}>
-								<img
-									className={styles.image}
-									src={playlist.cover}
-									alt="Обложка Плейлиста"
-									height="250"
-								/>
-								<p className={styles.text}>{playlist.title}</p>
-								<Link
-									className={styles.button}
-									to={`/playlists/${playlist.id}`}
-								>
-									Открыть плейлист
-								</Link>
-							</div>
-						</li>
-					))}
-				</ul>
+				filteredPlaylists?.length === 0 ? (
+					<p className={styles.result}>Ничего не найдено</p>
+				) : (
+					<ul className={styles.list}>
+						{filteredPlaylists?.map((playlist) => (
+							<li className={styles.item} key={playlist.id}>
+								<div className={styles.card}>
+									<img
+										className={styles.image}
+										src={playlist.cover}
+										alt="Обложка Плейлиста"
+										height="250"
+									/>
+									<p className={styles.text}>
+										{playlist.title}
+									</p>
+									<Link
+										className={styles.button}
+										to={`/playlists/${playlist.id}`}
+									>
+										Открыть плейлист
+									</Link>
+								</div>
+							</li>
+						))}
+					</ul>
+				)
 			) : null}
 			{status === "loading" ? <Spinner /> : null}
 		</div>
